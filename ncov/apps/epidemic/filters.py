3299de2df545081803260f713d2636089b97376f
jiaxin <edison7500@gmail.com>
2020-02-12 19:31:15 +0800

    update filter

--- ncov/apps/epidemic/filters.py
+++ ncov/apps/epidemic/filters.py
@@ -3,8 +3,9 @@ from apps.epidemic.models import Epidemic
 
 
 class EpidemicFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="name")
     published_at = filters.DateFilter(field_name="published_at")
 
     class Meta:
         models = Epidemic
-        fields = ["published_at"]
+        fields = ["name", "published_at"]
