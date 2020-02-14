ee891a35ea81dfa936a6c1e886bd1bade51ecf9e
jiaxin <edison7500@gmail.com>
2020-02-14 20:07:38 +0800

    update

--- ncov/ncov/settings/base.py
+++ ncov/ncov/settings/base.py
@@ -79,25 +79,3 @@ AUTH_PASSWORD_VALIDATORS = [
 # ----------------------------------------------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = "/static/"
-
-# REST FRAMEWORK
-# ----------------------------------------------------------------------------------------------------------------------
-# http://www.django-rest-framework.org/api-guide/settings/
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
-        "rest_framework.filters.OrderingFilter",
-        "rest_framework.filters.SearchFilter",
-    ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 30,
-    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
-}
---
+++ ncov/ncov/settings/prod.py
@@ -0,0 +1,28 @@
+from .base import *  # noqa
+from .base import env
+
+SECRET_KEY = env("SECRET_KEY")
+
+ALLOWED_HOSTS = ["*"]
+
+# REST FRAMEWORK
+# ----------------------------------------------------------------------------------------------------------------------
+# http://www.django-rest-framework.org/api-guide/settings/
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "apps.ext.rest.pagination.PageNumberPagination",
+    "PAGE_SIZE": 30,
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
+}
